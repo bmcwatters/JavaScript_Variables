be7790d7808014bbab788f95ab0fec51df9afbec
bryan mcwatters <bmcwatters@comcast.net>
2021-12-24 08:59:49 -0500

    completed exercise

--- app.js
+++ app.js
@@ -4,7 +4,21 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Bryan";
+let lastName = "McWatters";
+var age = 50;
 
 // Exercise 2
+let fullName = firstName + "" + lastName;
+console.log(fullName);
 
 // Exercise 3
+let city = "York";
+let passTime = "Tennis";
+let myStory = `Hi! My name is ${fullName}. I live in ${city}. I enjoy ${passTime} and coding!`;
+console.log(myStory);
+
+
+
+
+
